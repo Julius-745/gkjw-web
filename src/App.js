50afdb3861169c5f0247df25ed536e758e4c0454
Julius-745 <54503854+Julius-745@users.noreply.github.com>
2022-08-08 20:22:45 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,38 +2,26 @@ import React from 'react';
 import {
   ChakraProvider,
   Box,
+  Image,
   Text,
   Link,
   VStack,
   Code,
   Grid,
-  theme,
+  Stack,
+  Container,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import Banner  from './Component/Majelis.svg';
+import { Header } from './Component/Header';
+import { Footer } from './Component/Footer';
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
+    <ChakraProvider>
+      <Box>
+        <Header />
+        <Image src={Banner}/>
+        <Footer />
       </Box>
     </ChakraProvider>
   );
---
+++ src/Component/Header.js
@@ -0,0 +1,33 @@
+import {
+    Box,
+    Button,
+    Container,
+    Flex,
+    HStack,
+    Image,
+    Spacer,
+    useColorModeValue,
+  } from '@chakra-ui/react'
+  import * as React from 'react'
+  import Logo from './logos.svg'
+
+  
+  export const Header = () => {
+    return (
+      <Box pb={{ base: '12', md: '' }} >
+        <Box boxShadow={useColorModeValue('md', 'md-dark')}>
+          <Container maxW={"100%"} py={{ base: '5', lg: '5' }}>
+            <Flex px={[2,3]}>
+              <Box>
+                <Image src={Logo} alt="Logo"/>
+              </Box>
+              <Spacer />
+              <Box>
+                <Button colorScheme='facebook'>Contact Person</Button>
+              </Box>
+            </Flex>
+          </Container>
+        </Box>
+      </Box>
+    )
+  }
