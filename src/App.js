a6f1ba2d098d4a19c59a616761d499560ba8fad9
benedictus_bangkit <benedictusbangkit@gmail.com>
2022-08-18 00:05:29 +0900

    merge

--- src/App.js
+++ src/App.js
@@ -14,27 +14,21 @@ import {
 import { Banner }  from './Component/Banner';
 import { Header } from './Component/Header';
 import { Footer } from './Component/Footer';
-<<<<<<< HEAD
 import { CardSection } from './Component/CardSection';
 import {Carousel} from './Component/Carousel';
-=======
 import { CardJadwalSection } from './Component/CardJadwalSection';
 import { CardIbadahLalu } from './Component/CardIbadahLalu';
->>>>>>> 97a8795cd5bd42ecbffea34a29e6d1101117521e
 
 function App() {
   return (
     <ChakraProvider>
       <Box>
         <Header />
-<<<<<<< HEAD
         <Banner />
         <Carousel />
         <CardSection/>
-=======
         <CardJadwalSection/>
         <CardIbadahLalu/>
->>>>>>> 97a8795cd5bd42ecbffea34a29e6d1101117521e
         <Footer />
       </Box>
     </ChakraProvider>
