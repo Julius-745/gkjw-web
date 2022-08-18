0f44b67c20345b3a7f517f4b815a8a4dac469d31
benedictus_bangkit <benedictusbangkit@gmail.com>
2022-08-18 22:48:53 +0900

    Set carousel API

--- src/App.js
+++ src/App.js
@@ -15,12 +15,14 @@ import { Banner }  from './Component/Banner';
 import { Header } from './Component/Header';
 import { Footer } from './Component/Footer';
 import { CardSection } from './Component/CardSection';
+import { Carousel } from './Component/Carousel';
 
 function App() {
   return (
     <ChakraProvider>
       <Box>
         <Header />
+        <Carousel />
         <CardSection/>
         <Footer />
       </Box>
