d4c3003478ccd848669b3b45c1c69a3937f5d3d4
Julius-745 <54503854+Julius-745@users.noreply.github.com>
2022-08-19 14:05:21 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,14 +16,17 @@ import { Header } from './Component/Header';
 import { Footer } from './Component/Footer';
 import { CardSection } from './Component/CardSection';
 import { Carousel } from './Component/Carousel';
+import { CardJadwalSection } from './Component/CardJadwalSection';
+import { CardIbadahLalu } from './Component/CardIbadahLalu';
 
 function App() {
   return (
     <ChakraProvider>
       <Box>
         <Header />
+        <CardJadwalSection />
         <Carousel />
-        <CardSection/>
+        <CardIbadahLalu/>
         <Footer />
       </Box>
     </ChakraProvider>
