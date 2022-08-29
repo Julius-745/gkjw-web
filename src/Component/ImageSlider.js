d6887d2a5da5fe747926ca84125cdf450d9d65f6
Julius-745 <54503854+Julius-745@users.noreply.github.com>
2022-08-30 01:51:42 +0700

    fix design and content render carousel

--- src/Component/ImageSlider.js
+++ src/Component/ImageSlider.js
@@ -1,20 +1,26 @@
-import { Image,Box } from "@chakra-ui/react";
+import { Image,Box, Text, Flex } from "@chakra-ui/react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const ImageSlider = ({ slides }) => {
   return (
     <Carousel infiniteLoop>
-      {slides.map((slide) => {
+      {slides.data?.map((slide) => {
         return (
+          <Flex justifyContent={'center'} alignItems={'center'} >
             <Box
-            height="300px" 
-            width="100%"
+            height={388} 
+            width={396}
             display="flex"
+            marginRight={16}
             >
-                <Image src={slide.image} />
-                <Image src={slide.image}/>
+              <Image w={396} h={388} src={slide.attributes.image?.data.attributes.url}/>
             </Box>
+            <Flex flexDirection={'column'} w={'568px'} textAlign={'left'}>
+              <Text color={'black'} fontWeight={'bold'}>{slide.attributes.title}</Text>
+              <Text color={'black'} paddingTop={5}>{slide.attributes.content}</Text>
+            </Flex>
+          </Flex>
         )
       })}
     </Carousel>
