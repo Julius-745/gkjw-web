dd3fa10bb5b16cb4ce0a3c6e591b24c69f740070
Julius-745 <54503854+Julius-745@users.noreply.github.com>
2022-09-01 00:11:42 +0700

    base detail article

--- src/Component/Article.js
+++ src/Component/Article.js
@@ -17,31 +17,29 @@ import {
 
     useEffect(() => {
         axios
-            .get("https://gkjwprob.domcloud.io/api/articles?populate=*")
-            .then(res => setArticle(res.data))
+            .get("https://gkjwprob.domcloud.io/api/articles/1?populate=*")
+            .then(res => setArticle(res.data.data.attributes))
             .catch(err => setError(err.message));
     } , []);
-
     
     if(error) {
         return <div>An error occured: {error.message}</div>
     }
+    
     return(
-        article.data?.map(article => (
             <Flex paddingTop={10} paddingLeft={16} paddingBottom={10} flexDirection={'column'}>
             <Box h={'81px'} textAlign={'center'}>
-                <Text fontSize={'4xl'} fontWeight={'bold'}>{article.attributes?.category}</Text>
-                <Text fontSize={'xl'}>{article.attributes.createdAt}</Text>
+                <Text fontSize={'4xl'} fontWeight={'bold'}>{article.category}</Text>
+                <Text fontSize={'xl'}>{article.createdAt}</Text>
             </Box>
             <Box textAlign={'left'} paddingTop={20}>
-                <Text fontSize={'3xl'} fontWeight={'bold'}>{article.attributes.title}</Text>
+                <Text fontSize={'3xl'} fontWeight={'bold'}>{article.title}</Text>
                 <Text fontSize={'xl'}>Ayat</Text>
             </Box>
             <Box display={'flex'} alignItems="center" flexDirection={'column'} paddingTop={45}>
-                <Image src={article.attributes.image?.data.attributes.url}/>
-                <Text fontSize={'md'} textAlign={'left'} maxW={'1128px'} paddingTop={25}>{article.attributes.content}</Text>
+                <Image src={article.image?.data.attributes.url}/>
+                <Text fontSize={'md'} textAlign={'left'} maxW={'1128px'} paddingTop={25}>{article.content}</Text>
             </Box>
         </Flex>
-        ))
     )
   }
