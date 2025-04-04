85b4a7aab0130e76a39a30d01b29638b84e62371
Julius-745 <juliusagung123@gmail.com>
2025-04-04 14:31:37 +0700

    fix build error

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,5 @@
 import { INavigation } from "@/types";
 import { Menu } from "lucide-react";
-import { Separator } from "@/components/ui/separator";
 import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
 import { Link } from "react-router";
 import Logo from '../assets/image/gkjw.svg'
