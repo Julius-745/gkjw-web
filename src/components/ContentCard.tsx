f9766d438abcd5e40877f79d82dfe2a53ffb416e
Julius-745 <juliusagung123@gmail.com>
2025-05-03 14:41:25 +0700

    fix: content card

--- src/components/ContentCard.tsx
+++ src/components/ContentCard.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { ArticleCardProps } from "@/types";
+import { formatFullDate } from "@/lib/date";
 
-const ContentCard: React.FC<ArticleCardProps> = ({ imageUrl, title, source, description, onClick }) => {
+const ContentCard: React.FC<ArticleCardProps> = ({ imageUrl, title, article_category, description, createdAt, onClick }) => {
+
   return (
     
     <Card className="w-full shadow-md rounded-xl overflow-hidden cursor-pointer hover:shadow-lg transition-shadow duration-300" onClick={onClick}>
@@ -10,8 +12,9 @@ const ContentCard: React.FC<ArticleCardProps> = ({ imageUrl, title, source, desc
         <img src={imageUrl} alt={title} className="w-28 h-28 object-cover rounded-lg" />
         <div>
           <h3 className="text-orange-500 font-semibold text-lg">
-            {title} <span className="text-gray-500"> - {source}</span>
+            {title} <span className="text-gray-500"> - {String(article_category?.nama_kategori || '')}</span>
           </h3>
+          <p className="text-black-700 text-sm mt-2">{formatFullDate(createdAt)}</p>
           <p className="text-gray-600 text-sm mt-2">{description}</p>
         </div>
       </CardContent>
